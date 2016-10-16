3bbb272de539e553c198924a325a709273888547
anik545 <anik545@gmail.com>
2016-10-16 18:15:53 +0100

    can show hide individual lines

--- static/scripts/loci_plot.js
+++ static/scripts/loci_plot.js
@@ -1,67 +1,92 @@
-var board_atts={boundingbox:[-6,6,6,-6],
-    axis:true,
-    pan:{enabled:true,
-        needshift:false,
-        needTwoFingers:true
+var w = $('#box').width();
+var h = $('#box').height();
+var x_ax=w/20;
+var y_ax=h/20;
+var board_atts = {
+        boundingbox: [-x_ax, y_ax, x_ax, -y_ax],
+        axis: true,
+        pan: {
+            enabled: true,
+            needshift: false,
+            needTwoFingers: true
         },
-    zoom:{
-        factorX:1.25,
-        factorY:1.25,
-        wheel:true,
-        needshift:false
+        zoom: {
+            factorX: 1.25,
+            factorY: 1.25,
+            wheel: true,
+            needshift: false
         }
     }
-//var board = JXG.JSXGraph.initBoard('box', board_atts);
 
-var plots = 0 //Use plots variable to track each plot (name of each curve)
-//A better function plotter
-//http://jsxgraph.uni-bayreuth.de/wiki/index.php/Even_simpler_function_plotter
-
-//Hide/show elements from checkboxes
-//https://groups.google.com/forum/#!topic/jsxgraph/-AAkjV4CTDw
+var plots = -1
+    //A better function plotter
+    //http://jsxgraph.uni-bayreuth.de/wiki/index.php/Even_simpler_function_plotter
 
 //Use Board.removeObject() to delete individual lines
+var lines = []
 
 var board = JXG.JSXGraph.initBoard('box', board_atts);
 
-var arg = function arg(x,y){
-    if (x>0){
-        return Math.atan(y/x);
-    } else if (x<0 && y>=0) {
-        return Math.atan(y/x)+Math.PI;
-    } else if (x<0 && y<0) {
-        return Math.atan(y/x)-Math.PI;
-    } else if (x===0 && y>0){
-        return Math.PI/2;
-    } else if (x===0 && y<0){
-        return -1*Math.PI/2;
+var arg = function arg(x, y) {
+    if (x > 0) {
+        return Math.atan(y / x);
+    } else if (x < 0 && y >= 0) {
+        return Math.atan(y / x) + Math.PI;
+    } else if (x < 0 && y < 0) {
+        return Math.atan(y / x) - Math.PI;
+    } else if (x === 0 && y > 0) {
+        return Math.PI / 2;
+    } else if (x === 0 && y < 0) {
+        return -1 * Math.PI / 2;
     }
     return null
 };
 
-
-$(document).ready(function(){
-    $('#go').on('click',function(){
-        $.getJSON($SCRIPT_ROOT+'/_plot',{
-            eq:$('input[name="in"]').val()
-        },function(data){
-            console.log(data.result);
-            $.each(data.result,function(i,v){
-                f=board.jc.snippet(v,true,'x',true);
-                curve=board.create('functiongraph',[f],{name:plots,withLabel:false});
-            })
-            plots+=1
-        });
-        return false; //--> //if fail? it was in the example
-        /*console.log(board);
-        var input=$('#eq_in').val();
-        f=board.jc.snippet(input,true,'x',true);
-        curve=board.create('functiongraph',[f],{name:input,withLabel:true});
-        plots+=1;*/
+$(document).on('change','[type=checkbox]',function(){
+    console.log(this);
+    var id=$(this).attr('id')
+    console.log(id,plots,lines[id]);
+    $.each(lines[id], function(i,v){
+        vis = !v.getAttribute('visible');
+        v.setAttribute({visible:vis});
     });
-    $('#clear').on('click',function(){
-        plots=0;
+});
+
+$(document).ready(function() {
+    $('#go').on('click', function() {
+        var eq = $('input[name="in"]').val()
+        $.getJSON($SCRIPT_ROOT + '/_plot', {
+            eq: eq
+        }, function(data) {
+            plots += 1;
+            lines.push([]);
+            $('#expressions tbody').append(
+                '<tr>'+
+                    '<td>'+
+                        eq+
+                    '</td>'+
+                    '<td>'+
+                        '<input type="checkbox" name="plot" id="'+plots+'" checked>'+
+                    '</td>'+
+                '</tr>'
+                )
+            $.each(data.result, function(i, v) {
+                f = board.jc.snippet(v, true, 'x', true);
+                curve = board.create('functiongraph', [f], {
+                    name: plots,
+                    withLabel: false
+                });
+                lines[plots].push(curve);
+            });
+        });
+        return false;
+    });
+    $('#clear').on('click', function() {
+        $('#expressions tbody > tr').remove();
+        plots = -1;
+        lines=[];
         JXG.JSXGraph.freeBoard(board);
         board = JXG.JSXGraph.initBoard('box', board_atts);
     });
+
 });
